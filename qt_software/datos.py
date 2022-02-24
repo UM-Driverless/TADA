a1b1d07963825b73a4cf82957e92dadb7e73dd76
Jositote <ja.sebasdavila@gmail.com>
2022-02-24 22:31:06 +0100

    Definicion de paquetes
    - Se han definido 7 tipos de paquetes de datos
    - Se han añadido funciones para decodificar los mismos
    - Optimizacion del codigo

--- qt_software/datos.py
+++ qt_software/datos.py
@@ -5,52 +5,105 @@ def datosPantalla(self):
         if (primeraVez == 0):
             end_serial.reset_input_buffer()
             primeraVez = 1
-        
-        global rpm
-        global ect
-        global oilP
-        global tps
-        global apps
-        global breakHdr
-
-
-        global arrayDatos
+            
         i = 0
         while(end_serial.in_waiting > 0 and i < 12)  :
             #se lee la informacion y se guarda en una variable
-            arrayDatos.append(end_serial.read().decode())
+            arrayDatos.append(end_serial.read())
             print("Nuevo dato: ",arrayDatos)
             i += 1
 
-    
-        global SOF
-        global EOF
         datoActual = 0
 
         while (len(arrayDatos) != 0):
             print("Array while: ", arrayDatos)
+
             if (arrayDatos[0] == SOF and datoActual == 0):
                 print("deco SOF")
                 datoActual = 1
+                arrayDatos.pop(0)
+
+            if(datoActual == 1 and len(arrayDatos) > 10):
+                if (arrayDatos[10] == EOF):
+                    if (arrayDatos[0] == varPack1):
+                        print("pack1")
+                        pack = arrayDatos[1:9]
+                        rpm = int(str(pack[3])+str(pack[2])+str(pack[1])+str(pack[0]))
+                        ect = int(str(pack[5])+str(pack[4]))
+                        lambd4 = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0)  
+                    
+                    elif (arrayDatos[0] == varPack2):
+                        print("pack2")
+                        pack = arrayDatos[1:9]
+                        ect = int(str(pack[1])+str(pack[0]))
+                        fuelPressure = int(str(pack[3])+str(pack[2]))
+                        tps = int(str(pack[5])+str(pack[4]))
+                        apps = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0)
+                    
+                    elif (arrayDatos[0] == varPack3):
+                        
+                        print("pack3")
+                        pack = arrayDatos[1:9]
+                        pack = arrayDatos[1:9]
+                        batteryVoltage = int(str(pack[1])+str(pack[0]))
+                        breakPreassure = int(str(pack[3])+str(pack[2]))
+                        airTemp = int(str(pack[5])+str(pack[4]))
+                        OilPreassure = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0) 
+                    
+                    elif (arrayDatos[0] == varPack4):
+                        print("pack4")
+                        pack = arrayDatos[1:9]
+                        velocidadActual = int(str(pack[1])+str(pack[0]))
+                        velocidadObjetivo = int(str(pack[3])+str(pack[2]))
+                        anguloActual = int(str(pack[5])+str(pack[4]))
+                        anguloObjetivo = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0)  
+                    
+                    elif (arrayDatos[0] == varPack5):
+                        print("pack5")
+                        pack = arrayDatos[1:9]
+                        frenoActual = int(str(pack[1])+str(pack[0]))
+                        frenoObjetivo = int(str(pack[3])+str(pack[2]))
+                        motorActual = int(str(pack[5])+str(pack[4]))
+                        motorObjetivo = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0)      
+                    
+                    elif (arrayDatos[0] == varPack6):
+                        print("pack6")
+                        pack = arrayDatos[1:9]
+                        asState = int(str(pack[0]))
+                        ebsState = int(str(pack[1]))
+                        missionSelected = int(str(pack[2]))
+                        steeringState = int(str(pack[3]))
+                        breakState = int(str(pack[4]))
+                        lapCounter = int(str(pack[5]))
+                        conosActuales = int(str(pack[7])+str(pack[6]))
+                        for i in range(10):
+                            arrayDatos.pop(0)    
+                    
+                    elif (arrayDatos[0] == varPack7):
+                        print("pack7")
+                        pack = arrayDatos[1:9]
+                        asms = int(str(pack[0]))
+                        goSignal = int(str(pack[1]))
+                        for i in range(10):
+                            arrayDatos.pop(0)  
 
                 arrayDatos.pop(0)
-            elif (arrayDatos[0] == varPack1.id and datoActual == 1 and len(arrayDatos) > 10):
-                if (arrayDatos[10] == EOF):
-                    print("pack1 SOF")
-                    pack = arrayDatos[1:9]
-                    rpm = int(str(pack[1])+str(pack[0]))
-                    ect = int(str(pack[2])+str(pack[3]))
-                    oilP = int(pack[4])
-                    tps = int(pack[5])
-                    apps = int(pack[6])
-                    breakHdr = int (pack[7])
-                    for i in range(10):
-                        arrayDatos.pop(0)         
-                arrayDatos.pop(0)
                 datoActual = 0
+
             elif (len(arrayDatos) > 12):
                 datoActual = 0
                 arrayDatos.pop(0)
+
             else: break
 
 
--- qt_software/variables.py
+++ qt_software/variables.py
@@ -8,56 +8,52 @@ end_serial = serial.Serial(port='COM3', baudrate=9600, bytesize=8, parity='N', s
 
 primeraVez = 0
 
-SOF = '8'
-EOF = '8'
+SOF = 0x01
+EOF = 0x04
 
+#pack1:
+varPack1 = 0x31
 rpm = 0
 ect = 0
-oilP = 0
+lambd4 = 0
+
+varPack2 = 0x32
+map = 0
+fuelPressure = 0
 tps = 0
 apps = 0
-breakHdr = 0
 
-class varPack1:
-    id = '1'
-    
-    def modifyRpm(var):
-        global rpm
-        rpm = var
+varPack3 = 0x33
+batteryVoltage = 0
+breakPreassure = 0
+airTemp = 0
+OilPreassure = 0
 
-    def modifyEct(var):
-        global ect
-        ect = var
+varPack4 = 0x34
+velocidadActual = 0
+velocidadObjetivo = 0
+anguloActual = 0
+anguloObjetivo = 0
 
-    def modifyOilP(var):
-        global oilP
-        oilP = var
+varPack5 = 0x35
+frenoActual = 0
+frenoObjetivo = 0
+motorActual = 0
+motorObjetivo = 0
 
-    def modifyTps(var):
-        global tps
-        tps = var
+varPack6 = 0x36
+asState = 0
+ebsState = 0
+missionSelected = 0
+steeringState = 0
+breakState = 0
+lapCounter = 0
+conosActuales = 0
 
-    def modifyApps(var):
-        global apps
-        apps = var
-
-    def modifyBreakHdr(var):
-        global breakHdr
-        breakHdr = var
-
-dvState = 0
-
-class varPack2:
-    id = '2'
-
-    def modifyDvState(var):
-        global dvState
-        dvState = var
-
-#pack3
-#pack4
-#pack5
-
+varPack7 = 0x37
+asms = 0
+goSignal = 0
+pack7 = [varpack7,asms,goSignal]
 
 
 funcionError = 0
